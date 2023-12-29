fbe10ee1ed52ed919ce33380f61cb64c3a1a9d6c
kyungjae-Y <yoonkj2008@gmail.com>
2023-12-29 16:48:33 +0900

    2023.12.29
    미완성 프로젝트

---
+++ Project_YKJ/src/Main/Game.java
@@ -0,0 +1,12 @@
+package Main;
+
+public class Game {
+	public static final int WIDTH = 1280;
+	public static final int HEIGHT = 720;
+	public static final int NOTE_SPEED = 15;
+	public static final int SLEEP_TIME = 10;
+
+	public static void main(String[] args) {
+		new GameFrame();
+	}
+}
